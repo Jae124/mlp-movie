5672ec7edd7220116fbdde4f5d1b62eb7d8092a6
Valmik <valmikkpatel@gmail.com>
2017-12-12 00:20:32 -0500

    error-handling-new-model
    decoupled preprocessing and predict. also includes error handling.

--- server/hi.py
+++ server/hi.py
@@ -5,6 +5,7 @@ import os
 os.chdir("..")
 sys.path.insert(0,os.path.abspath(os.curdir))
 from predict_rating import predict_rating
+from preprocess import preprocess
 
 from model import ModelHandler
 
@@ -18,19 +19,27 @@ class PredictHandler(RequestHandler):
         self.set_header('Access-Control-Allow-Origin', 'http://localhost:3000')
         self.set_header('Access-Control-Allow-Credentials', 'true')
     def get(self):
-        runtime = int(self.get_argument('runtime'))
+        
+        runtime = self.get_argument('runtime')
+        
         director = str(self.get_argument('director'))
         genres = self.get_argument('genres')
-       
-        g = genres.split(",")   
+        actors = self.get_argument('actors')
 
+        g = genres.split(",")   
+        a = actors.split(",")
 
         ######### process in the modeling script?
         poster = str(self.get_argument('image_url'))
 
         ########### switch 'model.predict()' w/method call name equivalent to our model
-        rating = predict_rating(director, g, runtime, poster)
-        self.write('{}'.format(rating))
+        x,img = preprocess(director, g, runtime, poster, a)
+
+        if x is None:
+            self.write('{}'.format(img))
+        else:
+            rating = predict_rating(x,img)
+            self.write('{}'.format(rating))
 
 class ModelApplication(Application):
     def __init__(self, handler_mapping):
