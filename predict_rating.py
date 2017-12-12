46156f4ec4755193675590ea17d7e505b5f72d2b
Valmik <valmikkpatel@gmail.com>
2017-12-12 00:25:13 -0500

    updated-predict-rating

--- predict_rating.py
+++ predict_rating.py
@@ -7,18 +7,12 @@ import urllib
 def predict_rating(x,img):
 
 	#director = 'Joss Whedon'
-<<<<<<< HEAD
 	#director = 'Director_'+director_1
-=======
-	director = 'Director_'+director_1
-
->>>>>>> origin/master
 	#genres = ['Action','Adventure','Sci-Fi']
 	#runtime = 143
 	#img_url = 
 	#filename = 'test.jpg'
 
-<<<<<<< HEAD
 	#train = pd.read_csv('data/movie_train.csv',index_col=0)
 	#x = train.iloc[0]
 	#x[director] = 1
@@ -26,20 +20,10 @@ def predict_rating(x,img):
 	#for g in genres:
 	#	if g != "":
 	#		x[g] = 1
-=======
-	train = pd.read_csv('data/movie_train.csv',index_col=0)
-	#train = pd.read_csv('data/movie_train_2.csv',index_col=0)
-	x = train.iloc[0]
-	x[director] = 1
-
-	for g in genres:
-		if g != "":
-			x[g] = 1
-
+	
 	#for a in actors:
 		#if a != "":
 			#x['Actors_'+a] = 1	
->>>>>>> origin/master
 	
 	#x['Runtime']=runtime
 	#x.drop('rating',axis=0,inplace=True)
@@ -60,15 +44,10 @@ def predict_rating(x,img):
 	from keras.models import load_model
 
 	model = load_model('models/cnn.h5')
-<<<<<<< HEAD
 	#rfg = joblib.load('models/rfg.pkl')
 	rfg = joblib.load('models/rfg_2.pkl')
-=======
-	rfg = joblib.load('models/rfg.pkl')
-	#rfg = joblib.load('models/rfg_2.pkl')
->>>>>>> origin/master
 	lr = joblib.load('models/lr.pkl')
-
 	x1 = rfg.predict(x)
 	x2 = model.predict(img)
+	
 	return lr.predict([np.append(x1,x2.flatten())])
