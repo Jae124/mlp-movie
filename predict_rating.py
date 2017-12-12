2aebe197a85e795e70e6f8be0476d10a0504ba7b
Valmik <valmikkpatel@gmail.com>
2017-12-12 00:19:37 -0500

    decoupled-error-handling
    decoupled preprocessing and predict. also includes error handling.

--- predict_rating.py
+++ predict_rating.py
@@ -4,16 +4,29 @@ from sklearn.externals import joblib
 import urllib
 
 
-def predict_rating(director_1, genres, runtime, img_url):
+def predict_rating(x,img):
 
 	#director = 'Joss Whedon'
+<<<<<<< HEAD
+	#director = 'Director_'+director_1
+=======
 	director = 'Director_'+director_1
 
+>>>>>>> origin/master
 	#genres = ['Action','Adventure','Sci-Fi']
 	#runtime = 143
 	#img_url = 
-	filename = 'test.jpg'
+	#filename = 'test.jpg'
 
+<<<<<<< HEAD
+	#train = pd.read_csv('data/movie_train.csv',index_col=0)
+	#x = train.iloc[0]
+	#x[director] = 1
+
+	#for g in genres:
+	#	if g != "":
+	#		x[g] = 1
+=======
 	train = pd.read_csv('data/movie_train.csv',index_col=0)
 	#train = pd.read_csv('data/movie_train_2.csv',index_col=0)
 	x = train.iloc[0]
@@ -26,28 +39,34 @@ def predict_rating(director_1, genres, runtime, img_url):
 	#for a in actors:
 		#if a != "":
 			#x['Actors_'+a] = 1	
+>>>>>>> origin/master
 	
-	x['Runtime']=runtime
-	x.drop('rating',axis=0,inplace=True)
-	x = x.reshape((1,3875))
-	from PIL import Image
-	from scipy.misc import imresize
+	#x['Runtime']=runtime
+	#x.drop('rating',axis=0,inplace=True)
+	#x = x.reshape((1,3875))
+	#from PIL import Image
+	#from scipy.misc import imresize
 
 
-	urllib.urlretrieve(img_url, filename)
+	#urllib.urlretrieve(img_url, filename)
 
-	img = Image.open("test.jpg")
-	img = np.array(img)
-	img = imresize(img,(225,150,3))
-	img = img*1.0/256
-	img = np.array([img])
+	#img = Image.open("test.jpg")
+	#img = np.array(img)
+	#img = imresize(img,(225,150,3))
+	#img = img*1.0/256
+	#img = np.array([img])
 
 	import keras
 	from keras.models import load_model
 
 	model = load_model('models/cnn.h5')
+<<<<<<< HEAD
+	#rfg = joblib.load('models/rfg.pkl')
+	rfg = joblib.load('models/rfg_2.pkl')
+=======
 	rfg = joblib.load('models/rfg.pkl')
 	#rfg = joblib.load('models/rfg_2.pkl')
+>>>>>>> origin/master
 	lr = joblib.load('models/lr.pkl')
 
 	x1 = rfg.predict(x)
---
+++ preprocess.py
@@ -0,0 +1,50 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.externals import joblib
+import urllib
+def preprocess(director_1, genres, runtime, img_url, actors):
+
+	
+	
+	filename = 'test.jpg'
+
+	#train = pd.read_csv('data/movie_train.csv',index_col=0)
+	train = pd.read_csv('data/movie_train_2.csv',index_col=0)
+	x = train.iloc[0]
+	
+	if director_1 != "":
+		director = 'Director_'+director_1
+		x[director] = 1
+
+	for g in genres:
+		if g != "":
+			x[g] = 1
+	
+	for a in actors:
+ 		if a != "":
+ 			x['Actors_'+a] = 1
+	
+	try:
+		runtime = int(runtime)
+		if runtime <= 0:
+			return None, " runtime "
+		x['Runtime']=runtime
+	except:
+		return None, " runtime "
+	x.drop('rating',axis=0,inplace=True)
+	x = x.reshape((1,3875))
+	from PIL import Image
+	from scipy.misc import imresize
+
+	try:
+		urllib.urlretrieve(img_url, filename)
+		img = Image.open("test.jpg")
+		img = np.array(img)
+		img = imresize(img,(225,150,3))
+		img = img*1.0/256
+		img = np.array([img])
+	except:
+		return None, " url "
+
+
+	return x,img
